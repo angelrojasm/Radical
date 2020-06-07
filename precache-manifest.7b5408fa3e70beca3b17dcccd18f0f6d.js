78fdaccbfa41500c0cfec1fc858ace751499eb55
angelrojasm <angelrojasm6@gmail.com>
2020-06-07 01:06:35 -0400

    Updates

---
+++ precache-manifest.7b5408fa3e70beca3b17dcccd18f0f6d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b459f1165a3caffb4d862c45a96910a8",
+    "url": "/Radical/index.html"
+  },
+  {
+    "revision": "5ebf74bfa3fa9d1e43e4",
+    "url": "/Radical/static/css/main.0473f58b.chunk.css"
+  },
+  {
+    "revision": "cc18e18c314707a2a8fe",
+    "url": "/Radical/static/js/2.e6f502bd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Radical/static/js/2.e6f502bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ebf74bfa3fa9d1e43e4",
+    "url": "/Radical/static/js/main.8f67ccb4.chunk.js"
+  },
+  {
+    "revision": "3755914104630d5c5a0d",
+    "url": "/Radical/static/js/runtime-main.f8c9825e.js"
+  },
+  {
+    "revision": "e039c6049af4d6b2ef497dac741e89d4",
+    "url": "/Radical/static/media/IMG_0193.e039c604.jpg"
+  },
+  {
+    "revision": "80a60abd3f3c2cce5512e40c9fe6e511",
+    "url": "/Radical/static/media/IMG_0273.80a60abd.jpg"
+  },
+  {
+    "revision": "c27e1840c34fa25c76521d0c40f8f44f",
+    "url": "/Radical/static/media/IMG_0751.c27e1840.jpg"
+  },
+  {
+    "revision": "fffb4b42153ae0fa9117d06362101e07",
+    "url": "/Radical/static/media/IMG_0759.fffb4b42.jpg"
+  },
+  {
+    "revision": "4a3d3473e0cff92a49ce2251f6a9e80b",
+    "url": "/Radical/static/media/IMG_1095.4a3d3473.jpg"
+  },
+  {
+    "revision": "4ebc29c9446a8bd8f9378bcf3be250ba",
+    "url": "/Radical/static/media/IMG_1659.4ebc29c9.jpg"
+  },
+  {
+    "revision": "7caa98d775cc0294847034cc3ae8ff5b",
+    "url": "/Radical/static/media/logo-white-revised.7caa98d7.png"
+  }
+]);
