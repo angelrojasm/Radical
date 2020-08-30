6e4fb57fc6a4e660f6267d612dad3c6b475d4b44
angelrojasm <angelrojasm6@gmail.com>
2020-08-30 01:58:34 -0400

    modified backend server for serving static site

--- packages/server/app.js
+++ packages/server/app.js
@@ -10,6 +10,10 @@ app.use(cors());
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('../client/build'));
 }
+
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+});
 app.listen(process.env.PORT || port, () => {
 	console.log('app is running on port ' + port);
 });
