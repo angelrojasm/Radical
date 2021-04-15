5fd9ac6186803c557a4c9141ad49daa221ef70b6
angelrojasicq <arojas@icq247.com>
2021-04-15 13:21:39 -0400

    Updated build

---
+++ packages/client/build/precache-manifest.eb0e4cc210e59095a0aa68dabecf5c47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ead22707be2b3f5bee011f7ebffcfe21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89f649481065c33eed7f",
+    "url": "/static/css/2.9f0d004c.chunk.css"
+  },
+  {
+    "revision": "f882ea581ca57aed3ec6",
+    "url": "/static/css/main.84414835.chunk.css"
+  },
+  {
+    "revision": "89f649481065c33eed7f",
+    "url": "/static/js/2.c6e322b7.chunk.js"
+  },
+  {
+    "revision": "17089c0625a17dcf4977430f49df4440",
+    "url": "/static/js/2.c6e322b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f882ea581ca57aed3ec6",
+    "url": "/static/js/main.517a1bc8.chunk.js"
+  },
+  {
+    "revision": "ecae7d91ad7abe304c0c",
+    "url": "/static/js/runtime-main.8540fbb3.js"
+  }
+]);
