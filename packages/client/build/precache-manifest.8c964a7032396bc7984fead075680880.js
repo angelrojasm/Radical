1288f89d4d6584326fdad9889dd2ce0018e5437b
angelrojasm <angelrojasm6@gmail.com>
2021-04-17 21:54:37 -0400

    Updated build folder

---
+++ packages/client/build/precache-manifest.8c964a7032396bc7984fead075680880.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a2291291c94a0363c64ad728a55a564",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8f903d20e66fb50ff83",
+    "url": "/static/css/2.f1b50ea8.chunk.css"
+  },
+  {
+    "revision": "c3bb0cd239c2513008a4",
+    "url": "/static/css/main.44aab036.chunk.css"
+  },
+  {
+    "revision": "d8f903d20e66fb50ff83",
+    "url": "/static/js/2.36f0dd85.chunk.js"
+  },
+  {
+    "revision": "7e8661572dbf62ca66d7837115f59618",
+    "url": "/static/js/2.36f0dd85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3bb0cd239c2513008a4",
+    "url": "/static/js/main.a564201b.chunk.js"
+  },
+  {
+    "revision": "87c5611fed5572c1b154",
+    "url": "/static/js/runtime-main.5d1a68ce.js"
+  }
+]);
--- packages/client/build/service-worker.js
+++ packages/client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eb0e4cc210e59095a0aa68dabecf5c47.js"
+  "/precache-manifest.8c964a7032396bc7984fead075680880.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
