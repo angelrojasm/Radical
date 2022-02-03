e8662ef84c6abc6f54dbb45e677055946df96b6f
angelrojasm <angelrojasm6@gmail.com>
2022-02-03 19:00:32 -0400

    configuring support for loose coupling front/back end services

--- packages/server/app.js
+++ packages/server/app.js
@@ -1,36 +1,27 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3001;
-const path = require("path");
-const userRouter = require("./routers/userRouter");
-const itemRouter = require("./routers/itemRouter");
-const cartRouter = require("./routers/cartRouter");
-const likesRouter = require("./routers/likesRouter");
-const orderRouter = require("./routers/orderRouter");
-const fileUpload = require("express-fileupload");
-const adminRouter = require("./routers/adminRouter");
+const userRouter = require('./routers/userRouter');
+const itemRouter = require('./routers/itemRouter');
+const cartRouter = require('./routers/cartRouter');
+const likesRouter = require('./routers/likesRouter');
+const orderRouter = require('./routers/orderRouter');
+const fileUpload = require('express-fileupload');
+const adminRouter = require('./routers/adminRouter');
 
 app.use(express.json());
 app.use(cors());
 app.use(fileUpload());
-app.use("/user", userRouter);
-app.use("/item", itemRouter);
-app.use("/cart", cartRouter);
-app.use("/likes", likesRouter);
-app.use("/order", orderRouter);
-app.use("/admin", adminRouter);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
-}
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"));
-});
+app.use('/user', userRouter);
+app.use('/item', itemRouter);
+app.use('/cart', cartRouter);
+app.use('/likes', likesRouter);
+app.use('/order', orderRouter);
+app.use('/admin', adminRouter);
 
 app.listen(process.env.PORT || port, () => {
-  console.log("app is running on port " + port);
+	console.log('app is running on port ' + port);
 });
 
 module.exports = app;
